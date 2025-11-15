a4e987b302ac8b82bbbd36509aa244b6d8688be9
Stefany Giovanetti <ste_giovanetti@yahoo.it>
2025-11-15 15:52:47 +0100

    terminato esercizio

--- resources/views/articoli/articleDetail.blade.php
+++ resources/views/articoli/articleDetail.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Capybath-Il nostro blog</title>
+        <title>Cappyblog-Articolo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="/style.css">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-grigio text-bianco">
+        <nav class="navbar navbar-expand-lg bg-sabbia">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#"><img id="logoNavbar" src="/media/logo.png" alt="Logo del sito" class="logo" width="60" height="60"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,16 +20,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link nav-custom" aria-current="page" href="{{route('homepage')}}">Home</a>
+                            <a class="nav-link" aria-current="page" href="{{route('homepage')}}">Homepage</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-custom" href="{{route('chi.siamo')}}">Chi siamo</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-custom" href="{{route('service')}}">Servizi</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-custom" href="{{route('blog')}}">Blog</a>
+                            <a class="nav-link" href="{{route('blog')}}">Blog</a>
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
@@ -42,7 +36,7 @@
         <div class="container-fluid bg-body-secondary bg-white-background">
             <div class="row h-75 justify-content-center align-items-center">
                 <div class="col-12">
-                    <h1 class="text-center display-4 shadow text-grigio">{{$article['description']}}</h1>
+                    <h1 class="text-center display-4 shadow text-brown">{{$article['description']}}</h1>
                 </div>
             </div>
             <div class="row justify-content-center align-items-center">
@@ -50,7 +44,7 @@
                     <img src="{{$article['img']}}" alt="" class="img-cappy">
                 </div>
                 <div class="col-12 col-md-6">
-                    <p class="text-grigio"> {{$article['articolo']}}</p>
+                    <p class="text-brown"> {{$article['articolo']}}</p>
                 </div>
             </div>
         </div>
